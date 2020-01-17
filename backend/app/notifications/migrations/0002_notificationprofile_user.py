395a7f55903e1c74466eeb26aed01c8e2ebc9eac
roncaglionidaniele <roncaglionidaniele@icloud.com>
2020-01-17 11:47:24 +0100

    fixture

--- backend/app/notifications/migrations/0002_notificationprofile_user.py
+++ backend/app/notifications/migrations/0002_notificationprofile_user.py
@@ -10,14 +10,14 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('registration', '0001_initial'),
+        ('notifications', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='registrationprofile',
+            model_name='notificationprofile',
             name='user',
-            field=models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, related_name='registration_profile', to=settings.AUTH_USER_MODEL, verbose_name='user'),
+            field=models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, related_name='notification_profile', to=settings.AUTH_USER_MODEL, verbose_name='user'),
         ),
     ]
