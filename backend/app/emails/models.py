f2162816b0b205f1493dfa78e2a5f82b1383914a
roncaglionidaniele <roncaglionidaniele@icloud.com>
2020-01-23 15:13:53 +0100

    sentry integration and send_email bug fix

--- backend/app/emails/models.py
+++ backend/app/emails/models.py
@@ -81,9 +81,8 @@ class EmailType(models.Model):
 
 
 @receiver(send_email)
-def send_email(sender, request, to, email_type, code, **kwargs):
+def send_email(sender, request, to, email_type, **kwargs):
     # signals only purpose in the registration module is to extract logo_url, otherwise could just call task in serializer.
     logo_url = request.build_absolute_uri(settings.STATIC_URL)
-    kwargs['code'] = code
     kwargs.pop('signal', None)
     send_email_task.delay(logo_url, to, email_type, **kwargs)  # send async task to celery
--- backend/app/settings.py
+++ backend/app/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 import ast
 from datetime import timedelta
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 
@@ -193,3 +195,14 @@ CACHES = {
     }
 }
 #############################################################
+#############################################################
+# Error Monitoring with sentry
+if not DEBUG:
+    sentry_sdk.init(
+        dsn=os.environ.get('SENTRY_DSN'),
+        integrations=[DjangoIntegration()],
+
+        # If you wish to associate users to errors (assuming you are using
+        # django.contrib.auth) you may enable sending PII data.
+        send_default_pii=True
+    )
