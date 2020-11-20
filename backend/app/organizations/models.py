614a32d55ba399f2e2f0d3d709b239ccb0ac6975
Joseph Edwin Gill <josephedwingill@gmail.com>
2020-11-20 14:00:55 +0100

    setup Organization model and admin

--- backend/app/organizations/models.py
+++ backend/app/organizations/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+from app.groups.models import Group
+
+
+class Organization(models.Model):
+    name = models.CharField(
+        max_length=150
+    )
+
+    created = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    updated = models.DateTimeField(
+        auto_now=True
+    )
+
+    groups = models.ManyToManyField(
+        to=Group,
+        related_name='organizations'
+    )
+
+    def __str__(self):
+        return f'Organization #{self.pk}'
