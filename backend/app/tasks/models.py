67a511f1a6cf3a30105eb3668e30102666760bc9
Joseph Edwin Gill <josephedwingill@gmail.com>
2020-11-20 11:25:33 +0100

    setup Tasks admin and models, need to find out if responsible should be many-many or many-one field instead

--- backend/app/tasks/models.py
+++ backend/app/tasks/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    # This likely needs to be Many_to_One or Many_to_Many field tied to User_Profile, need more info from Alain
+    responsible_user = models.CharField()
+
+    planned_completion_date = models.DateField()
+
+    due_date = models.DateField()
+
+    description = models.CharField(
+        max_length=300
+    )
+
+    documents = models.FileField(
+        blank=True,
+        null=True
+    )
+
+    created = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    updated = models.DateField(
+        auto_now=True
+    )
+
+    def __str__(self):
+        return f'Task #{self.pk}'
