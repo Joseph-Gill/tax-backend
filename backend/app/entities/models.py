19a3e908ec8c20bf2b6fd8f2d75633430415af18
Joseph Edwin Gill <josephedwingill@gmail.com>
2020-11-20 11:02:51 +0100

    setup Entity admin and model, need to check required status of tax_rate / location / legal_form

--- backend/app/entities/models.py
+++ backend/app/entities/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+class Entity(models.Model):
+    # Stores the parent Id number of an entity, can have no pid making it the top most entity on a chart
+    pid = models.CharField(
+        max_length=150,
+        null=True,
+        blank=True
+    )
+
+    name = models.CharField(
+        max_length=150
+    )
+
+    # Can an Entity not have a Legal Form??
+    legal_form = models.CharField(
+        max_length=150,
+        null=True,
+        blank=True
+    )
+
+    # Can an Entity not have a location??
+    location = models.CharField(
+        max_length=150,
+        null=True,
+        blank=True
+    )
+
+    # Possibly a decimal number?? / Can an Entity not have a tax rate??
+    tax_rate = models.IntegerField()
+
+    created = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    updated = models.DateTimeField(
+        auto_now=True
+    )
+
+    def __str__(self):
+        return f'Entity #{self.pk}'
