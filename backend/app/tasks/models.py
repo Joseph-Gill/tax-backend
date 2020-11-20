55f2841a51b4464d2a8da93f47e1b83a177b89ca
Joseph Edwin Gill <josephedwingill@gmail.com>
2020-11-20 14:46:55 +0100

    removed designated_user from Task model, replaced with Foreign Key off UserProfile - assigned_user

--- backend/app/tasks/models.py
+++ backend/app/tasks/models.py
@@ -2,11 +2,6 @@ from django.db import models
 
 
 class Task(models.Model):
-    # This likely needs to be Many_to_One or Many_to_Many field tied to User_Profile, need more info from Alain
-    responsible_user = models.CharField(
-        max_length=150
-    )
-
     planned_completion_date = models.DateField()
 
     due_date = models.DateField()
