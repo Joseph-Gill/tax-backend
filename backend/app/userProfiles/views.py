c80b2125a519f766b70508cf9ee5fe48306088e6
Joseph Edwin Gill <josephedwingill@gmail.com>
2020-11-20 15:33:04 +0100

    setup base framework of userProfile view

--- backend/app/userProfiles/views.py
+++ backend/app/userProfiles/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework.generics import RetrieveUpdateAPIView, ListAPIView, UpdateAPIView
 
-# Create your views here.
+
+class RetrieveUpdateLoggedInUserProfile(RetrieveUpdateAPIView):
+    """
+    get:
+    Retrieve the logged in User's Profile
+
+    update:
+    Update the logged in User's Profile
+    """
+
+    pass
+
+
+class RetrieveLoggedInUserTasks(ListAPIView):
+    """
+    List the logged in User's Tasks
+    """
+
+    pass
+
+
+class UpdateSpecificUserGroupOrProjectRole(UpdateAPIView):
+    """
+    Update a specified User's Group or Project Role
+    """
+
+    pass
