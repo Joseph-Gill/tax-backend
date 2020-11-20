1312575e7a871202be57ea2d5df3fc453f6d4390
Joseph Edwin Gill <josephedwingill@gmail.com>
2020-11-20 13:50:36 +0100

    setup Group model and admin

--- backend/app/groups/models.py
+++ backend/app/groups/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+from app.entities.models import Entity
+from app.projects.models import Project
+
+
+class Group(models.Model):
+    name = models.CharField(
+        max_length=150
+    )
+
+    created = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    updated = models.DateTimeField(
+        auto_now=True
+    )
+
+    entities = models.ForeignKey(
+        to=Entity,
+        on_delete=models.CASCADE,
+        related_name='group',
+        null=True,
+        blank=True
+    )
+
+    projects = models.ForeignKey(
+        to=Project,
+        on_delete=models.CASCADE,
+        related_name='group',
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return f'Group #{self.pk}'
