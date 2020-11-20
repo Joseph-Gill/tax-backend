1b7291de14521aa75f708f4a2728281ce91858a7
Joseph Edwin Gill <josephedwingill@gmail.com>
2020-11-20 10:41:45 +0100

    setup Chart admin and model, need to find better Field to store slinks/clinks/nodes

--- backend/app/charts/models.py
+++ backend/app/charts/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Chart(models.Model):
+    # This needs to store an Array of Objects, possibly JSON??
+    clinks = models.CharField(
+        null=True,
+        blank=True
+    )
+
+    # This needs to store an Array of Objects, possibly JSON??
+    slinks = models.CharField(
+        null=True,
+        blank=True
+    )
+
+    # This needs to store an Array of Objects, possibly JSON??
+    nodes = models.CharField(
+        null=True,
+        blank=True
+    )
+
+    created = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    updated = models.DateTimeField(
+        auto_now=True
+    )
+
+    def __str__(self):
+        return f'Chart #{self.pk}'
