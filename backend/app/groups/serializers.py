0affcada96fb448ca1f523ed4e6992fed9ac3981
Joseph Edwin Gill <josephedwingill@gmail.com>
2020-11-24 14:34:54 +0100

    cleanup Group serializers.py

--- backend/app/groups/serializers.py
+++ backend/app/groups/serializers.py
@@ -1,11 +1,8 @@
 from rest_framework import serializers
-
 from app.groups.models import Group
-from app.userProfiles.models import UserProfile
 
 
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
         fields = ['id', 'name', 'avatar', 'created', 'updated', 'entities', 'projects', 'organizations', 'users']
-        # optional_fields = ['user']
