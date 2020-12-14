f0b3c1919855a87625095dfd969b15c97820fa38
Joseph Edwin Gill <josephedwingill@gmail.com>
2020-12-14 18:46:17 +0100

    Added GroupSerializer to ProjectSerializer to get additional info for Frontend

--- backend/app/projects/serializers.py
+++ backend/app/projects/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
+from app.groups.models import Group
 from app.projectRoles.serializers import ProjectRoleSerializer
 from app.projects.models import Project
 from app.steps.serializers import StepSerializer
+
+
+class ProjectGroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = ['id', 'name']
 
 
 class ProjectSerializer(serializers.ModelSerializer):
@@ -15,6 +22,10 @@ class ProjectSerializer(serializers.ModelSerializer):
         many=True
     )
 
+    group = ProjectGroupSerializer(
+        required=False
+    )
+
     class Meta:
         model = Project
-        fields = ['id', 'name', 'description', 'start_date', 'end_date', 'status', 'created', 'updated', 'steps', 'assigned_users_roles']
+        fields = ['id', 'name', 'description', 'start_date', 'end_date', 'status', 'created', 'updated', 'steps', 'assigned_users_roles', 'group']
