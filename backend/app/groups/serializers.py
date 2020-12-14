a175e24b8c308874b6bd5d4f0f8c52098e1e1b06
Joseph Edwin Gill <josephedwingill@gmail.com>
2020-12-14 21:30:59 +0100

    Added Registration info to the Group Serializer for the frontend

--- backend/app/groups/serializers.py
+++ backend/app/groups/serializers.py
@@ -4,7 +4,19 @@ from app.entities.serializers import EntitySerializer
 from app.groups.models import Group
 from app.organizations.serialziers import OrganizationSerializer
 from app.projects.serializers import ProjectSerializer
+from app.registration.models import RegistrationProfile
 from app.userProfiles.serializers import UserProfileSerializer
+from app.users.serializers import UserSerializer
+
+
+class GroupRegistrationSerializer(serializers.ModelSerializer):
+    user = UserSerializer(
+        required=False
+    )
+
+    class Meta:
+        model = RegistrationProfile
+        fields = ['user', 'code_used']
 
 
 class GroupSerializer(serializers.ModelSerializer):
@@ -28,6 +40,11 @@ class GroupSerializer(serializers.ModelSerializer):
         many=True
     )
 
+    invited_new_users = GroupRegistrationSerializer(
+        required=False,
+        many=True
+    )
+
     class Meta:
         model = Group
-        fields = ['id', 'name', 'avatar', 'created', 'updated', 'entities', 'projects', 'organizations', 'users']
+        fields = ['id', 'name', 'avatar', 'created', 'updated', 'entities', 'projects', 'organizations', 'users', 'invited_new_users']
