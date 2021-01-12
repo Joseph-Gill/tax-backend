7d743e6ee621d50ee3b1c71288a54ded7f3a036d
Joseph Edwin Gill <josephedwingill@gmail.com>
2021-01-12 14:35:23 +0100

    Adjust UserSerializer to provide additional info needed by frontend

--- backend/app/users/serializers.py
+++ backend/app/users/serializers.py
@@ -1,13 +1,24 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from app.userProfiles.models import UserProfile
 
 User = get_user_model()
 
 
+class UserUserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = ['id', 'country', 'phone_number']
+
+
 class UserSerializer(serializers.ModelSerializer):
+    user_profile = UserUserProfileSerializer(
+        required=False
+    )
+
     class Meta:
         model = User
-        fields = ['id', 'email', 'first_name', 'last_name', 'username']
+        fields = ['id', 'email', 'first_name', 'last_name', 'username', 'user_profile']
         read_only_fields = ['email']
 
 
