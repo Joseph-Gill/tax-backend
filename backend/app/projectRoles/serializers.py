8aa799916d45ef59ffbfd62c37a9cd7b4498d74e
Joseph Edwin Gill <josephedwingill@gmail.com>
2021-04-27 18:35:35 +0100

    Update ProjectRoleSerializer to provide required info for the frontend

--- backend/app/projectRoles/serializers.py
+++ backend/app/projectRoles/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from app.groups.models import Group
 from app.projectRoles.models import ProjectRole
 from app.projects.models import Project
 from app.userProfiles.models import UserProfile
@@ -7,7 +8,17 @@ from app.userProfiles.models import UserProfile
 User = get_user_model()
 
 
+class GroupProjectRoleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = ['id', 'name', 'avatar']
+
+
 class ProjectProjectRoleSerializer(serializers.ModelSerializer):
+    group = GroupProjectRoleSerializer(
+        required=False
+    )
+
     class Meta:
         model = Project
         fields = ['id', 'group', 'name']
