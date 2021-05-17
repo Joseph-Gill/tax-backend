fb44df39a5d17e445317a2416ac6b7672640b86e
Joseph Edwin Gill <josephedwingill@gmail.com>
2021-05-17 16:24:37 +0100

    Update Chart Serializer to provided data needed for Frontend

--- backend/app/charts/serializers.py
+++ backend/app/charts/serializers.py
@@ -1,9 +1,20 @@
 from rest_framework import serializers
 from app.charts.models import Chart
+from app.projects.models import Project
 from app.steps.models import Step
 
 
+class ChartProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ['id', 'name']
+
+
 class ChartStepSerializer(serializers.ModelSerializer):
+    project = ChartProjectSerializer(
+        required=False
+    )
+
     class Meta:
         model = Step
         fields = ['id', 'effective_date', 'number', 'project']
